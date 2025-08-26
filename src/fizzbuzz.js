457c8d04760b3cae952939c83b2d3e75f7e855d4
Aylin <aylinmjt10@gmail.com>
2025-08-25 18:12:50 -0600

    Agregue mi codigo de FizzBuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,22 @@
 export function fizzbuzz(){
-  //tu código va aqui
-  
+ 	var i;
+	for (i=1; i<=100; ++i) {
+		if (i%3==0 && i%5==0) {
+			console.log("FIZZ BUZZ!");
+		} else {
+			if (i%3==0) {
+				console.log("FIZZ!");
+			} else {
+				if (i%5==0) {
+					console.log("BUZZ!");
+				} else {
+					console.log(i);
+				}
+			}
+		}
+	}
+
 }
+
+fizzbuzz();
+
